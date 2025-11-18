acd5845f3ebcf01dc344252e117def98a3b7df50
Klara <klara.vandenburg@yahoo.com>
2025-11-18 15:48:49 +0800

    Initial commit for feature-add-spotify

--- she_codes_2025/jurassic-spark_frontend/src/components/NavBar.jsx
+++ she_codes_2025/jurassic-spark_frontend/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ function NavBar() {
     return (
         <div>
         <nav>
-            <Link to="/">Home-</Link>
+            <Link to="/">Home</Link>
             <Link to="/fundraiser">Fundraiser</Link>
         </nav>
         <Outlet />
